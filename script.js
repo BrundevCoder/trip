5b9ec5d1c013f23f6c96510de494db3eb425e1f6
BrundevCoder <brunodevdrums@gmail.com>
2026-05-20 22:53:42 +0100

    added every file

---
+++ script.js
@@ -0,0 +1,84 @@
+const container = document.getElementById("content");
+
+const tasks = [
+  {"Name": "Atividade na mente", "Status": "Completa", "description": "Esperando pela aprovação do Bruno ainda!", "complete": true},
+  //
+  {"Name": "Aprovação do Bruno", "Status": "Em Andamento", "description": "Esperando pela aprovação do Bruno ainda!", "complete": false},
+  //
+  {"Name": "Aprovação do pais do Bruno", "Status": "-", "description": "Esperando pela aprovação dos pais do Bruno ainda! :)", "complete": false},
+  //
+  {"Name": "Dia confirmado", "Status": "-", "description": "Ainda esperamos pela confirmação do dia oficial à visita!", "complete": false}
+];
+
+function showTasks(name, status, decp, complete) {
+
+  // create a section element
+  // status
+  const section = document.createElement("section");
+  section.classList.add("block")
+
+  const statusDiv = document.createElement("div");
+  statusDiv.classList.add("stats");
+
+  section.appendChild(statusDiv);
+
+  const para = document.createElement("p");
+  para.classList.add("para")
+  const span = document.createElement("span");
+
+  statusDiv.appendChild(para)
+  para.innerText = "Status: ";
+
+  span.innerText = status;
+
+  para.appendChild(span);
+
+  // ball
+  const ball = document.createElement("div");
+  para.appendChild(ball);
+
+  ball.classList.add("ball");
+
+  // title
+  const title = document.createElement("h2")
+  section.appendChild(title);
+
+  title.classList.add("title");
+  title.innerText = name;
+
+  // descp
+  const description = document.createElement("p");
+  description.classList.add("description");
+
+  description.innerText = decp;
+
+  section.appendChild(description);
+
+  // check if it is complete
+  if (complete) {
+    section.classList.add("complete");
+    span.classList.add("span-completed");
+  }
+
+  if (status.includes("Andamento") && !complete) {
+    span.classList.add("span-in-progress");
+  }
+
+  // show it
+  container.appendChild(section);
+}
+
+function update() {
+  let len = tasks.length;
+
+  for (let i = 0; i < len; i++) {
+    let name = tasks[i]["Name"];
+    let status = tasks[i]["Status"];
+    let description = tasks[i]["description"];
+    let complete = tasks[i]["complete"];
+
+    showTasks(name, status, description, complete);
+  }
+}
+
+update();
